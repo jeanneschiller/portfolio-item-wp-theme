70e03c9e1c80a408bc093002a6f46a98af1132bb
Jeanne Schiller <schiller.jeanne@gmail.com>
2015-08-25 14:20:11 -0400

    gallery styling

--- gallery.php
+++ gallery.php
@@ -10,10 +10,14 @@ get_header();  ?>
   <div class="container">
 
     <div class="content gallery-container">
+      <div class="gallery-header">
+      <?php the_post_thumbnail('large'); ?>
+      <!--<h2><?php// the_title(); ?></h2>-->
+      </div>
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title(); ?></h2>
+        
         <?php the_content(); ?>
 
       <?php endwhile; // end the loop?>
